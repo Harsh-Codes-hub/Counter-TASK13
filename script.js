02ea5ecec61408726e39f8ea7ca60ff479dcdec6
Harsh Gajrani <harshgajrani23@gmail.com>
2025-12-29 20:06:04 +0530

    Add favicon images and update HTML to include them; enhance CSS styles and JavaScript functionality for counter application

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 let a = 0;
 let counterEl = document.getElementById("counter");
+let resetEl = document.getElementById("reset");
 let incEl = document.getElementById("inc");
 let decEl = document.getElementById("dec");
-
-incEl.addEventListener('click', function() {
-    a++;
-    counterEl.innerHTML = a;
-})
-decEl.addEventListener('click', function() {
-    a--;
-    counterEl.innerHTML = a;
-})
+resetEl.addEventListener("click", function () {
+  counterEl.innerHTML = "0";
+});
+incEl.addEventListener("click", function () {
+  a++;
+  counterEl.innerHTML = a;
+});
+decEl.addEventListener("click", function () {
+  a--;
+  counterEl.innerHTML = a;
+});
